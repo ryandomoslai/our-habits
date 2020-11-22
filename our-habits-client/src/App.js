852020bf26311b25c3e92f557ea8f628375004e7
rjd471 <rjd471@mail.usask.ca>
2020-11-21 23:56:49 -0600

    Added router

--- our-habits-client/src/App.js
+++ our-habits-client/src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
 import axios from 'axios';
 
 function App() {
@@ -38,33 +44,79 @@ function App() {
   }
 
   return (
-      <div>
-        <h2>Welcome to my app</h2>
-        <form onSubmit={handleSubmit}>
-          <div className={'form-input'}>
-            <input
-              type={'text'}
-              value={title}
-              onChange={event => setTitle(event.target.value)}
-              placeholder={'input'}
-            />
-          </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
 
-          <div className={'form-input'}>
-            <textarea name={'body'} placeholder={'textarea'} cols={'30'} rows={'10'} value={textArea} onChange={event => setTextArea(event.target.value)} />
-          </div>
-
-          {posts.map(post => (
-              <div>
-                {post.title}
-              </div>
-          ))}
-
-
-          <button>Submit</button>
-        </form>
-      </div>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
+
+  function Home() {
+    return <h2>Home</h2>;
+  }
+
+  function About() {
+    return <h2>About</h2>;
+  }
+
+  function Users() {
+    return <h2>Users</h2>;
+  }
+
+  // return (
+  //     <div>
+  //       <h2>Welcome to my app</h2>
+  //       <form onSubmit={handleSubmit}>
+  //         <div className={'form-input'}>
+  //           <input
+  //             type={'text'}
+  //             value={title}
+  //             onChange={event => setTitle(event.target.value)}
+  //             placeholder={'input'}
+  //           />
+  //         </div>
+  //
+  //         <div className={'form-input'}>
+  //           <textarea name={'body'} placeholder={'textarea'} cols={'30'} rows={'10'} value={textArea} onChange={event => setTextArea(event.target.value)} />
+  //         </div>
+  //
+  //         {posts.map(post => (
+  //             <div>
+  //               {post.title}
+  //             </div>
+  //         ))}
+  //
+  //
+  //         <button>Submit</button>
+  //       </form>
+  //     </div>
+  // );
 }
 
 export default App;
