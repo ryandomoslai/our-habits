a3811f57d2b989175021ef027344bf1ad693daab
ryandomoslai <ryanwork277@gmail.com>
2020-11-24 00:10:15 -0600

    removed unused api

--- server.js
+++ server.js
@@ -23,45 +23,6 @@ mongoose.connection.on('connected', () => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const newHabitPost = new HabitPost({
-//     habitName: 'Learn French',
-//     username: 'Jean',
-//     content: 'Test post from Jean',
-// });
-// newHabitPost.save();
-// newHabitPost.save();
-
-
-// const findCondition = { 'username': 'Ryan', 'habitName': 'Learn French' };
-// const HabitScore = require('./models/habitScore');
-// HabitScore.findOne(findCondition).then((result) => {
-//     const update = {
-//         currentStreak: result.currentStreak += 3,
-//         totalScore: result.totalScore += 3
-//     };
-//     if (update.currentStreak > result.bestStreak) {
-//         update.bestStreak = update.currentStreak;
-//     }
-//
-//     HabitScore.findOneAndUpdate(findCondition, update, { useFindAndModify: false }, (error, result) => {
-//
-//     });
-// })
-// HabitScore.findOneAndUpdate(findCondition, update, { useFindAndModify: false }, (error, result) => {
-//     console.log(result);
-//     console.log(error);
-// });
-
-
-// const Habit = require('./models/habit');
-// const newHabitScore = new Habit({
-//     icon: 'default',
-//     name: 'lower followers',
-//     description: 'For those who want to make their bed',
-//     followers: 2
-// });
-// newHabitScore.save();
-
 // HTTP request logger
 app.use(morgan('tiny'));
 
